01d7f535040c2e9fe9d84c8f03d4116582c8e1f0
Nathan K <nathankidd@hey.com>
2026-04-05 16:33:10 +0000

    Fix TodoRead: add placeholder param to satisfy Anthropic input_schema requirement

--- lib/brute/tools/todo_read.rb
+++ lib/brute/tools/todo_read.rb
@@ -5,6 +5,7 @@ module Brute
     class TodoRead < LLM::Tool
       name "todo_read"
       description "Read the current todo list to check task status and progress."
+      param :_placeholder, String, "Unused, pass any value"
 
       def call
         {todos: Brute::TodoStore.all}
