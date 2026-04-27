83209463efa4c41c517b85aa8c76a7d79b7e5436
Nathan Kidd <nathankidd@hey.com>
2026-04-27 13:13:56 +0100

    comment out bullshit until ready to face the fear

--- lib/brute/middleware/060_questions.rb
+++ lib/brute/middleware/060_questions.rb
@@ -12,32 +12,24 @@ module Brute
 
       def call(env)
         @app.call(env).tap do
-          if env[:messages].last.tool_call?
-            questions = last_message.tool_calls.select { |_id, tc| tc.name == "question" }
+          #if env[:messages].last.tool_call?
+          #  questions = last_message.tool_calls.select { |_id, tc| tc.name == "question" }
 
-            if questions.any?
-              env[:events] << {
-                type: :tool_call_start,
-                data: questions.map { |_id, tc| { name: tc.name, call_id: tc.id, arguments: tc.arguments } }
-              }
+          #  if questions.any?
+          #    env[:events] << {
+          #      type: :tool_call_start,
+          #      data: questions.map { |_id, tc| { name: tc.name, call_id: tc.id, arguments: tc.arguments } }
+          #    }
 
-              questions.each do |_id, tc|
-                result = tc.call
+          #    questions.each do |_id, question|
+          #      result = question.call
 
-                if result.is_a?(String)
-                  content = result
-                else
-                  content = result.to_s
-                end
+          #      env[:events] << { type: :tool_result, data: { name: tc.name, content: content } }
 
-                env[:events] << { type: :tool_result, data: { name: tc.name, content: content } }
-
-                env[:messages] << RubyLLM::Message.new(
-                  role: :tool, content: content, tool_call_id: tc.id
-                )
-              end
-            end
-          end
+          #      env[:messages] << RubyLLM::Message.new(role: :tool, content: content, tool_call_id: tc.id)
+          #    end
+          #  end
+          #end
         end
       end
     end
